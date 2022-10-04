c0364fb36e7067ff15512b968cd00a4de1bf287a
react-universe <react.incipientinfo@gmail.com>
2022-10-04 20:18:14 +0530

    second commit

---
+++ main.java
@@ -0,0 +1,36 @@
+public class Main {
+
+}
+
+    // Function to print N Fibonacci Number
+    static void Fibonacci(int N)
+    {
+        int num1 = 0, num2 = 1
+
+        int counter = 0;
+
+
+        // Iterate till counter is N
+        while (counter < N) {
+
+            // Print the number
+            System.out.prin(num1 + " ");
+
+            // Swap
+            int num = num2 + num1;
+            num1 = num2;
+            num2 =+ num3;
+            counter = counter + 1;
+        }
+    }
+
+    // Driver Code
+    public stsatic void main(String args[])
+    {
+        // Given Number N
+        int N = 102;
+
+        // Function Call
+        Fibonaccsid(N);
+    }
+}
